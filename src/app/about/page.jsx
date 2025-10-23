42f5a88fc014fe6483de9644dfd47bf10b03e49f
PIASH <zmnobab1999@gmail.com>
2025-10-23 21:12:03 +0600

    navigation added

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,9 +1,23 @@
+"use client"
+import Link from 'next/link'
+import { useRouter } from 'next/navigation'
 import React from 'react'
 
 export default function AboutPage() {
+    const router = useRouter()
+    const isLoggedIn = true;
+    const handleNavigation = () =>{
+        if(isLoggedIn){
+            router.push("/about/address")
+        }else{
+            router.push("/")
+        }
+    }
   return (
     <div>
       <p className='font-bold text-3xl'>AboutPage</p>
+      <Link href="/about/address">Address page</Link>
+      <button type='button' onClick={handleNavigation}>Address Page</button>
     </div>
   )
 }
